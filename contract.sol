3f703d8a9bfee73c544cea361b512805667d25a3
Ponzi Trade <116192051+PonziTrade@users.noreply.github.com>
2022-10-19 19:40:48 +0330

    create contract.sol
    create contract.sol

---
+++ contract.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+// This is a Ponzi smart contract that was used in the www.ponzi.trade project. The main difference between this Ponzi project and others is that in this project the admin does not have any access to your funds. When you want to withdraw your profit, 20% of the amount will be paid to the admin wallet with the address [0x865f51332e458Ba52204496d9Bd609Df65067847]. And 10% will be paid to your referrer. As long as the project has new investors, you can withdraw your profits, and if not, you should wait for new investors to join.
+
+pragma solidity ^0.8.16;
+
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool);
+
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
+library SafeMath {
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a, "SafeMath: subtraction overflow");
+        uint256 c = a - b;
+        return c;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+        return c;
+    }
